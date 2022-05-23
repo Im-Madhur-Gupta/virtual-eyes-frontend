75d61f987772429cf714937865dfbfb7be69739d
Madhur Gupta <madhurgupta107@gmail.com>
2022-05-23 11:27:33 +0530

    removes uneccessary dependencies

--- App.js
+++ App.js
@@ -1,24 +1,19 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
 import { StyleSheet, Text, View } from "react-native";
 
 import DescribeImage from "./src/components/DescribeImage";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text
-        style={{
-          color: "lightblue",
-          fontSize: 50,
-          padding: 10,
-        }}
-      >
-        Virtual Eyes
-      </Text>
-      <DescribeImage />
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Text>Virtual Eyes</Text>
+        <DescribeImage />
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
   );
 }
 
